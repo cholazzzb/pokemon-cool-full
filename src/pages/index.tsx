57f98fc1e23db944937d342e146806633f6f5383
nictoro <nicholas.biantoro@ruangguru.com>
2022-07-07 22:45:19 +0700

    refactor: detailed page folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import { FC, useState, Dispatch, SetStateAction, Fragment } from 'react';
 import Layout from '@/components/Layout';
 import Body from '@/components/Body';
 import Listpage from '@/app/container/pages/Listpage/Listpage';
-import Detailpage from '@/components/Detailpage/Detailpage';
+import Detailpage from '@/app/container/pages/Detailpage/Detailpage';
 import Ownedpage from '@/app/container/pages/Ownedpage/Ownedpage';
 import Navigator from '@/components/Navigator';
 import { DETAILPAGE, LISTPAGE, OWNEDPAGE } from '@/constants/route';
