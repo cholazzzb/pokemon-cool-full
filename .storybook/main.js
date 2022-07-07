60594be7398599137a52c1f8fb504801bfba0ff0
nictoro <nicholas.biantoro@ruangguru.com>
2022-07-07 20:28:26 +0700

    feat: init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': '@emotion/react',
+          'emotion-theming': '@emotion/react',
+          '@': path.resolve(__dirname, '../src/'),
+        },
+      },
+    };
+  },
+};
