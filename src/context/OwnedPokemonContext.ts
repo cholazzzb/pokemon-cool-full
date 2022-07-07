3f5cb5b79619fee530609b08beadc3e13e08ecd7
nictoro <nicholas.biantoro@ruangguru.com>
2022-07-07 20:31:26 +0700

    refactor: remove context provider

--- src/context/OwnedPokemonContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export type OwnedPokemonContextType = {
-  ownedPokemon: any[];
-  savePokemon: (id: number, pokemonName: string, name: string) => void;
-  releasePokemon: (name: string) => void;
-};
-
-export const OwnedPokemonContext = createContext({} as OwnedPokemonContextType);
