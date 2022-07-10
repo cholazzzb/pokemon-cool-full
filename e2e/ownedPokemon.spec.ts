65b4d524307416691bd0409eee90f224159d8e99
nictoro <nicholas.biantoro@ruangguru.com>
2022-07-10 18:01:21 +0700

    refactor: change session storage implementation

--- e2e/ownedPokemon.spec.ts
+++ e2e/ownedPokemon.spec.ts
@@ -1,16 +1,16 @@
 // https://github.com/uchagani/new-project/blob/main/tests/example.spec.ts
 
-import OwnedPokemon from '@/utils/OwnedPokemon';
 import { test } from '@playwright/test';
+import { addPokemon } from '@/domains/ownedPokemon/ownedPokemonUtil';
 
 test('should show pokemon list', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  let ownedPokemonMem = new OwnedPokemon([]);
-  ownedPokemonMem.addPokemon(1, 'Bulbasaur', 'dino');
+  const ownedPokemons = {};
+  const { result } = addPokemon(ownedPokemons, 1, 'Bulbasaur', 'dino');
   await page.evaluate((ownedPokemon) => {
     window.sessionStorage.setItem('ownedPokemon', JSON.stringify(ownedPokemon));
-  }, ownedPokemonMem.data);
+  }, result);
 
   await page.reload();
   await page.locator('text=Owned').click();
