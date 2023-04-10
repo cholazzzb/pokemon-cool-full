715bad74971a574206724ddf21a8925605e9a0d2
cholazzzb <nicsphehehe@gmail.com>
2023-04-10 21:20:59 +0700

    improve(ui): pokemon-type detail

--- src/domains/type/typeLogic.ts
+++ src/domains/type/typeLogic.ts
@@ -1,30 +1,35 @@
+import { PokemonType } from '@/domains/pokemonType/pokemonTypeEntity';
 import { EffectivityMapKey } from './typeEntity';
 import { GetTypeDetailByIdRes } from './typeService.gql';
 
-type PokemonType = {
+type PokemonTargetType = {
   id: number;
-  name: string;
+  name: PokemonType;
 };
+
+export type RelationSummary = Array<
+  [EffectivityMapKey, Array<PokemonTargetType>]
+>;
 
 export const calculateTypeRelationSummary = (
   relation: GetTypeDetailByIdRes['pokemon_v2_type'][0]['pokemon_v2_typeefficacies'],
-): Array<[EffectivityMapKey, Array<PokemonType>]> => {
-  const map: Map<EffectivityMapKey, Array<PokemonType>> = new Map();
+): RelationSummary => {
+  const map: Map<EffectivityMapKey, Array<PokemonTargetType>> = new Map();
   for (const rel of relation) {
     const effectivity = rel.damage_factor as EffectivityMapKey;
 
     if (map.has(effectivity)) {
-      const types = map.get(effectivity) as Array<PokemonType>;
+      const types = map.get(effectivity) as Array<PokemonTargetType>;
       const newType = {
         id: rel.target_type_id ?? 0,
         name: rel.pokemonV2TypeByTargetTypeId?.name ?? '',
-      } as PokemonType;
+      } as PokemonTargetType;
       if (newType) map.set(effectivity, [...types, newType]);
     } else {
       const newType = {
         id: rel.target_type_id ?? 0,
         name: rel.pokemonV2TypeByTargetTypeId?.name ?? '',
-      } as PokemonType;
+      } as PokemonTargetType;
       if (newType) map.set(effectivity, [newType]);
     }
   }
--- src/pages/types/[typeId].tsx
+++ src/pages/types/[typeId].tsx
@@ -1,13 +1,12 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import Link from 'next/link';
 
 import { PokemonType } from '@/domains/pokemonType/pokemonTypeEntity';
 import { EffectivityMap } from '@/domains/type/typeEntity';
-import { calculateTypeRelationSummary } from '@/domains/type/typeLogic';
 import {
-  GetTypeDetailByIdRes,
-  getTypeDetailById,
-} from '@/domains/type/typeService.gql';
+  RelationSummary,
+  calculateTypeRelationSummary,
+} from '@/domains/type/typeLogic';
+import { getTypeDetailById } from '@/domains/type/typeService.gql';
 import { getPrimaryColorFromType } from '@/presentational/colorTheme';
 import Header from '@/presentational/components/Header';
 import {
@@ -18,37 +17,37 @@ import {
   YScrollable,
 } from '@/presentational/components/Layout';
 import Navigator from '@/presentational/components/Navigator';
-import TypeChip from '@/presentational/components/TypeChip';
+import PokemonTypeTag from '@/presentational/components/Tags/PokemonTypeTag';
 import TypeCard, { EffectivityText } from '@/presentational/type/TypeCard';
 import { convertURLQueryToString } from '@/utils/url';
 
 type TypeDetailPageProps = {
-  typeId: number;
-  typeDetail: GetTypeDetailByIdRes;
+  pokemonType: string;
+  relationSummary: RelationSummary;
 };
 
 const TypeDetailPage: NextPage<TypeDetailPageProps> = (props) => {
-  const detail = props.typeDetail.pokemon_v2_type[0];
-  const pokemonType = detail.name;
-  const color = getPrimaryColorFromType(pokemonType);
-  const relation = detail.pokemon_v2_typeefficacies;
-  const relationSummary = calculateTypeRelationSummary(relation);
+  const color = getPrimaryColorFromType(props.pokemonType);
 
   return (
     <Layout>
       <RightPane />
       <Body>
-        <Header caption={pokemonType} onClickBackLink="/types" />
-        <YScrollable style={{ backgroundColor: color, padding: '20px' }}>
-          {relationSummary.map((summary) => (
-            <TypeCard key={pokemonType}>
+        <Header caption={props.pokemonType} onClickBackLink="/types" />
+        <YScrollable style={{ backgroundColor: color, padding: 40 }}>
+          {props.relationSummary.map((summary) => (
+            <TypeCard key={props.pokemonType}>
               <EffectivityText>{EffectivityMap[summary[0]]}</EffectivityText>
 
-              <FlexWrap>
+              <FlexWrap css={{ justifyContent: 'space-around' }}>
                 {summary[1].map((type) => (
-                  <Link key={type.name} href={`/types/${type.id}`}>
-                    <TypeChip type={type.name as PokemonType} />
-                  </Link>
+                  <a
+                    key={type.name}
+                    href={`/types/${type.id}`}
+                    style={{ paddingBlockEnd: 8 }}
+                  >
+                    <PokemonTypeTag pokemonType={type.name as PokemonType} />
+                  </a>
                 ))}
               </FlexWrap>
             </TypeCard>
@@ -87,7 +86,12 @@ export const getStaticProps: GetStaticProps<TypeDetailPageProps> = async (
       throw new Error('getTypeDetailById Error');
     }
 
-    return { props: { typeId: Number(typeId), typeDetail } };
+    const detail = typeDetail.pokemon_v2_type[0];
+    const pokemonType = detail.name;
+    const relation = detail.pokemon_v2_typeefficacies;
+    const relationSummary = calculateTypeRelationSummary(relation);
+
+    return { props: { pokemonType, relationSummary } };
   } catch (error) {
     return {
       notFound: true,
